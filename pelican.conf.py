79521120f01019b1409168122f7daba2e5d17a54
Daniel Roseman <daniel@roseman.org.uk>
2014-10-13 13:38:04 +0100

    Updated settings.

--- pelican.conf.py
+++ pelican.conf.py
@@ -21,13 +21,18 @@ DEFAULT_LANG='en'
 SOCIAL = (('twitter', 'http://twitter.com/danielroseman'),
           ('github', 'http://github.com/danielroseman'),)
 
+RELATIVE_URLS = False
 DEFAULT_PAGINATION = 10
 CREATE_CLEAN_URLS = False
 LINK_CLEAN_URLS = True
 REVERSE_CATEGORY_ORDER = True
-ARTICLE_PERMALINK_STRUCTURE = '%Y/%m/%d/'
+#ARTICLE_PERMALINK_STRUCTURE = '%Y/%m/%d/'
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
 TWITTER_USERNAME = 'danielroseman'
 DISQUS_SITENAME = 'rosemanblog'
-THEME = 'notmyidea2'
+THEME = 'notmyidea'
 FEED_RSS = 'feeds/all.xml'
 TAG_FEED = 'feeds/tags/%s.xml'
+AUTHOR_URL = 'author/{slug}.html'
+GOOGLE_ANALYTICS = 'UA-11944054-1'
